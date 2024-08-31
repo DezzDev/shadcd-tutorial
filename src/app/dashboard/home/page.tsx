bd90b83876318771ca5df170ca74d768a49ddf0a
Daniel Zapata <daniel-dezz@hotmail.com>
2024-08-31 15:11:26 +0200

    instalacion de shadcd

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <div>
       <h1>Hello Page</h1>
+      <Button> button </Button>
     </div>
   );
 }
