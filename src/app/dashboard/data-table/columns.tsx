2382f9862de72bd4277adabcc85883e64c621db1
Daniel Zapata <daniel-dezz@hotmail.com>
2024-09-12 13:26:59 +0200

    sorting

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -8,12 +8,12 @@ import { Badge } from "@/components/ui/badge"
 import { MoreHorizontal, ArrowUp, ArrowDown } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
 
@@ -21,138 +21,142 @@ import { toast } from "sonner"
 
 
 type badgeVariant = {
-		variant:"secondary" | "default" | "success" | "destructive" 
-		| "outline" | "info" | null | undefined
-	} 
+	variant: "secondary" | "default" | "success" | "destructive"
+	| "outline" | "info" | null | undefined
+}
 
-const SortedIcon = ({isSorted}: {isSorted: SortDirection | boolean} ) => {
-	if (isSorted === "asc"){
+const SortedIcon = ({ isSorted }: { isSorted: SortDirection | boolean }) => {
+	if (isSorted === "asc") {
 		return <ArrowDown className="h-4 w-4" />
-	}else if (isSorted === "desc"){
+	} else if (isSorted === "desc") {
 		return <ArrowUp className="h-4 w-4" />
-	}else{
+	} else {
 		return null
 	}
 }
-	
+
 
 export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "clientName",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Client Name
+	{
+		accessorKey: "clientName",
+		header: ({ column }) => {
+			return (
+				
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					>
+						Client Name
+						<SortedIcon isSorted={column.getIsSorted()} />
+						{/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
+					</Button>
+				
+			)
+		},
+	},
+	{
+		accessorKey: "status",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Status
 					<SortedIcon isSorted={column.getIsSorted()} />
-          {/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
-        </Button>
-      )
-    },
-  },
-  {
-    accessorKey: "status",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Status
-					<SortedIcon isSorted={column.getIsSorted()} />
-          {/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
-        </Button>
-      )
-    },
-		cell: ({row})=>{
+					{/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
+				</Button>
+			)
+		},
+		cell: ({ row }) => {
 			const status = String(row.getValue("status"))
 			const variant = {
 				pending: "secondary",
 				processing: "info",
 				success: "success",
 				failed: "destructive"
-			}[status] ?? "secondary"  as any
+			}[status] ?? "secondary" as any
 
 			return (
 				<Badge variant={variant} capitalize>{status}</Badge>
 			)
 		}
-  },
+	},
 	{
 		accessorKey: "amount",
 		header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+			return (
+				<div className="text-right">
+					<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
 					className="text-right"
-        >
-          Amount
+				>
+					Amount
 					<SortedIcon isSorted={column.getIsSorted()} />
-          {/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
-        </Button>
-      )
-    },
+					{/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
+				</Button>
+				</div>
+			)
+		},
 		cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
+			const amount = parseFloat(row.getValue("amount"))
+			const formatted = new Intl.NumberFormat("en-US", {
+				style: "currency",
+				currency: "USD",
+			}).format(amount)
 
 			return <div className="text-right font-medium">{formatted}</div>
 
- 
-    },
+
+		},
 	},
-  {
-    accessorKey: "email",
-		header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-					<SortedIcon isSorted={column.getIsSorted()} />
-          {/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
-        </Button>
-      )
-    },
-  },
 	{
-    id: "actions",
-    cell: ({ row }) => {
-      const payment = row.original
- 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => {
+		accessorKey: "email",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					Email
+					<SortedIcon isSorted={column.getIsSorted()} />
+					{/* <ArrowUpDown className="ml-2 h-4 w-4" /> */}
+				</Button>
+			)
+		},
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const payment = row.original
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuItem
+							onClick={() => {
 								navigator.clipboard.writeText(payment.id)
-								toast.success("Copy",{
-									position:"top-right"
+								toast.success("Copy", {
+									position: "top-right"
 								})
 							}}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
-  },
+						>
+							Copy payment ID
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>View customer</DropdownMenuItem>
+						<DropdownMenuItem>View payment details</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
+	},
 ]
